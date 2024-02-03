d89433b0a624fb2882484f6685c16d38dcfe4af4
Nadin Perez <pereznadin6@gmail.com>
2024-02-03 16:26:09 -0300

    resolving problems

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d-flex justify-content-center mt-5">
+    <div class="spinner-border spinner-border-sm mt-1 me-2" role="status"></div>
+    <h5>Loading {{ name }}...</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'Data', 
+    },
+  },
+};
+</script>
+
+<style>
+/* Agrega estilos según sea necesario */
+</style>
